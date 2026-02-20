127170c74beebcfa8084f215c46f05c8ed0cc894
ManjulaDevi886 <manjuladevir886@gmail.com>
2026-02-20 10:13:27 +0530

    Delete frontend directory

--- frontend/src/hooks/useQueries.ts
+++
@@ -1,3 +0,0 @@
-// This file is intentionally minimal as the backend has no methods
-// All data is static and managed client-side
-export {};
--- frontend/src/pages/MenuPage.tsx
+++
@@ -1,9 +0,0 @@
-import MenuSection from '@/components/sections/MenuSection';
-
-export default function MenuPage() {
-    return (
-        <div>
-            <MenuSection />
-        </div>
-    );
-}
